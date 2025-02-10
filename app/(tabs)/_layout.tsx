a3a449ba653c79a6e75f245f9538a3d1c28e83d2
Nikolaas Verlee <niko.verlee@tailor-hub.com>
2025-02-10 13:34:18 +0100

    chore: exercise media url changed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 // app/(tabs)/_layout.tsx
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from '@/screens/HomeScreen';
 import ExercisesScreen from '@/screens/ExercisesScreen';
@@ -9,14 +10,28 @@ import AnalyticsScreen from '@/screens/AnalyticsScreen';
 
 const Tab = createBottomTabNavigator();
 
+const iconMap: Record<string, keyof typeof Ionicons.glyphMap> = {
+  Home: 'home-outline',
+  Exercises: 'barbell-outline',
+  Analytics: 'stats-chart',
+  'New Set': 'add-circle',
+};
+
 const Layout = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Exercises" component={ExercisesScreen} />
-      <Tab.Screen name="Analytics" component={AnalyticsScreen} />
-      <Tab.Screen name="New Set" component={NewSetScreen} />
-    </Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            const iconName = iconMap[route.name] || '';
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Exercises" component={ExercisesScreen} />
+        <Tab.Screen name="Analytics" component={AnalyticsScreen} />
+        <Tab.Screen name="New Set" component={NewSetScreen} />
+      </Tab.Navigator>
   );
 };
 
